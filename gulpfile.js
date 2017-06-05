d782e0f9213cb785df3a243d0e538ca135a7d7ac
joel-hamill <joel@mesosphere.io>
2017-06-05 15:19:37 -0700

    Add 1.10 version picker

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ const excerpts = require('metalsmith-excerpts')
 // general build settings
 //
 
-const docsVersions = ['1.7', '1.8', '1.9']
+const docsVersions = ['1.7', '1.8', '1.9', '1.10']
 const currentDevVersion = '1.10'
 
 const cssTimestamp = new Date().getTime()
